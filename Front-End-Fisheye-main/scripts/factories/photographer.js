a683ead864d057d82b15dccb29ba2b41bcb5e6d4
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-03-21 17:24:13 +0100

    Création des éléments de index.html via la fonction photographerFactory()

--- Front-End-Fisheye-main/scripts/factories/photographer.js
+++ Front-End-Fisheye-main/scripts/factories/photographer.js
@@ -4,14 +4,37 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        const a = document.createElement('a');
+        a.setAttribute('href', `photographer.html?&id=${id}`);
+        a.style.textDecoration = 'none';
+
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
+        const origin = document.createElement('p');
+        origin.textContent = `${city}, ${country}`;
+
+        const tag = document.createElement('p');
+        tag.setAttribute("class", "tagline");
+        tag.textContent = tagline;
+        
+        const prc = document.createElement('p')
+        prc.setAttribute('class', 'price');
+        prc.textContent = `${price}€/jour`
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        a.appendChild(article);
         article.appendChild(img);
+        article.appendChild(origin);  
+        article.appendChild(tag);
+        article.appendChild(prc)
         article.appendChild(h2);
-        return (article);
+      
+        return (a);
     }
     return { name, picture, getUserCardDOM }
 }
