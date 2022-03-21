3e4e0225b0932bb8f685ab508bfac7bd2031b0c2
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-03-22 00:04:43 +0100

    Créer la fonction getPhotographer() // pages/photographer.js

--- Front-End-Fisheye-main/scripts/pages/photographer.js
+++ Front-End-Fisheye-main/scripts/pages/photographer.js
@@ -1,1 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function getPhotographers() {
+    const respons = await fetch("data/photographers.json");
+
+    let data = await respons.json();
+    let photographers = [...data.photographers];
+    let dataMedia = [...data.media];
+
+    photographers.forEach((photographer) => {
+        const photographerMedias = dataMedia.filter((photographerMedia) => photographerMedia.photographerId === photographer.id);
+        photographer.medias = [...photographerMedias];
+    });
+    return { photographers };
+}
