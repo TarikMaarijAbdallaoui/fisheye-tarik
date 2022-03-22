e5584827859e4042fe2330fe95d665101afcc1c1
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-03-22 15:29:55 +0100

    Creation de la class PhotographerMediaImage // photographerMedia.js

--- Front-End-Fisheye-main/scripts/factories/photographerMedia.js
+++ Front-End-Fisheye-main/scripts/factories/photographerMedia.js
@@ -20,3 +20,26 @@ class PhotographerMediaFactory {
         else throw new Error('Unknown media type format');
     }
 }
+
+class PhotographerMediaImage {
+
+    constructor(media) {
+        this._photographerId = media.photographerId;
+        this._id = media.id;
+        this._title = media.title;
+        this._imageUrl = media.image;
+        this._totalLikes = media.likes;
+    }
+
+    get mediaUrl() {
+        return `assets/photographers/${this._photographerId}/${this._imageUrl}`;
+    }
+
+    get mediaType() {
+        return 'image';
+    }
+
+    get mediaCard() {
+        return `<img src="${this.mediaUrl}" title="${this._title}, closeup view" alt="${this._title}">`;
+    }
+}
