a27fa89e779833604ba03fe4a05b7f4a40a7efb3
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-03-22 15:31:42 +0100

    Creation de la class hotographerMediaVideo  // photographerMedia.js

--- Front-End-Fisheye-main/scripts/factories/photographerMedia.js
+++ Front-End-Fisheye-main/scripts/factories/photographerMedia.js
@@ -43,3 +43,26 @@ class PhotographerMediaImage {
         return `<img src="${this.mediaUrl}" title="${this._title}, closeup view" alt="${this._title}">`;
     }
 }
+
+class PhotographerMediaVideo {
+
+    constructor(media) {
+        this._photographerId = media.photographerId;
+        this._id = media.id;
+        this._title = media.title;
+        this._videoUrl = media.video;
+        this._totalLikes = media.likes;
+    }
+
+    get mediaUrl() {
+        return `assets/photographers/${this._photographerId}/${this._videoUrl}`;
+    }
+
+    get mediaType() {
+        return 'video';
+    }
+
+    get mediaCard() {
+        return `<video muted> <source src="${this.mediaUrl}#t=0.1" type="video/mp4" title="${this._title}, closeup view" alt="${this._title}"> Your browser does not support the video tag. </video>`;
+    }
+}
