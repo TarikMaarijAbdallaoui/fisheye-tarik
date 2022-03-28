2f20aa5c5c6f7a06a5c4a0296f5c1813a7e4aad3
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-03-28 10:52:19 +0200

    Création du  switch (mediaType) dans li lightboxFactory.js

--- Front-End-Fisheye-main/scripts/factories/lightboxFactory.js
+++ Front-End-Fisheye-main/scripts/factories/lightboxFactory.js
@@ -5,4 +5,28 @@ function setMediaModal(mediaId, mediaType, mediaUrl, mediaTitle){
 
   const modalTitleElement = document.querySelector(".media-modal-title p");
   modalTitleElement.textContent = mediaTitle;
+
+  switch (mediaType) {
+    case "image":
+      modalImgElement.setAttribute("src", mediaUrl);
+      modalImgElement.setAttribute("alt", mediaTitle);
+      modalVideoElement.style.display = "none";
+
+      modalImgElement.style.display = "flex";
+      modalImgElement.focus();
+      break;
+    case "video":
+      modalVideoElement.setAttribute("src", mediaUrl);
+      modalImgElement.style.display = "none";
+
+      modalVideoElement.setAttribute("alt", mediaTitle);
+      modalVideoElement.style.display = "flex";
+      modalVideoElement.focus();
+      break;
+    default:
+      throw new Error("Unknown media type format");
+  }
+
+  let actualMediaId;
+  actualMediaId = mediaId;
 }
