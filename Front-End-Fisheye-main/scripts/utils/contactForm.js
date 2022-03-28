fae226ece17e054f6a3ebe3fddea159fa0392451
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-03-28 02:58:50 +0200

    Création de la constante formTitle

--- Front-End-Fisheye-main/scripts/utils/contactForm.js
+++ Front-End-Fisheye-main/scripts/utils/contactForm.js
@@ -10,6 +10,9 @@ async function displayModal(){
     const modal = document.getElementById("contact_modal");
     modal.style.display = "flex";
     modal.style.position = "fixed";
+
+    const formTitle = document.querySelector("#contact_modal h2");
+    formTitle.innerHTML = `Contactez-moi <br> ${photographer.name}`;
 }
 
 function closeModal() {
